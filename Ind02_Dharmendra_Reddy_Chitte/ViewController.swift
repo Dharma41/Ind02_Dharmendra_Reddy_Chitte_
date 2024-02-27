12509139a45021c5ccb7a65076b676f3b3ed511f
Dharma41 <dharmendrareddy2000@gmail.com>
2024-02-27 14:09:30 -0600

    Label, Buttons added

--- Ind02_Dharmendra_Reddy_Chitte/ViewController.swift
+++ Ind02_Dharmendra_Reddy_Chitte/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Namelabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ShuffleLabel(_ sender: Any) {
+    }
+    
+    @IBAction func ShowAnswerLabel(_ sender: Any) {
+    }
 }
 
